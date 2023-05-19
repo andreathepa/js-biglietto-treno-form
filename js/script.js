c84dffff3121833a31560a0e97cdd715894bf416
andreathepa <andreadepa32@gmail.com>
2023-05-19 18:55:16 +0200

    exercise done

--- js/script.js
+++ js/script.js
@@ -2,12 +2,18 @@
 // Iniziamo implementando il programma senza alcuna estetica: usando esclusivamente due input e un bottone (non stilizzati), realizziamo le specifiche scritte sopra. La risposta finale (o output) sarà anch’essa da scrivere in console.
 
 let button = document.getElementById('invia');
+document.getElementById('km').value = '';
 
 button.addEventListener('click', function(){
 
     let km = parseInt(document.getElementById('km').value);
    
     let age = parseInt(document.getElementById('age').value);
+
+    let name = document.getElementById('name').value;
+
+    let discount_perc;
+
 
     let price = km * 0.21;
 
@@ -16,27 +22,38 @@ button.addEventListener('click', function(){
     if(isNaN(km) === false){
 
         if (age === 0){
-            message += `Il prezzo del biglietto è di: ${price.toFixed(2)}€`;
+
+            // message += `Il prezzo del biglietto è di: ${price.toFixed(2)}€`;
             discount_perc = '/';
         }
         else if (age === 1){
-            price *= 0.8;
             discount_perc = '20%';
+            price *= 0.8;
 
-            message += `Il prezzo del biglietto è di: ${price.toFixed(2)}€`;
+
+            // message += `Il prezzo del biglietto è di: ${price.toFixed(2)}€`;
         }
         else{
-            price *= 0.6;
             discount_perc = '40%';
+            price *= 0.6;
 
-            message += `Il prezzo del biglietto è di: ${price.toFixed(2)}€`;
+
+            // message += `Il prezzo del biglietto è di: ${price.toFixed(2)}€`;
         }
+
+        document.getElementById('show-name').innerText = name;
+        document.getElementById('show-km').innerText = km;
+        document.getElementById('show-age').innerText = age;
+        document.getElementById('show-discount').innerText = discount_perc;
+        document.getElementById('show-price').innerText = price;
 
 
     }
     else{
         message += 'Devi inserire i km da percorrere'
     }
+
+    
 
     document.getElementById('price').innerText = message;
 
